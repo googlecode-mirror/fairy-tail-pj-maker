46dc5ab0ecc2faea75e362dada40c05fa795ce1a
ladrillos.azules@gmail.com <ladrillos.azules@gmail.com@78e8bc3e-2a09-b054-e0e4-caeba71fdec4>
2014-02-06 16:45:47 +0000

    Ya no da errores, ahora no pasa la matriz ¡tengo que cambiar eso!

--- trunk/FairyTailvNetbeans/class_pj.php
+++ trunk/FairyTailvNetbeans/class_pj.php
@@ -41,35 +41,35 @@ class pj {
     }
     
     function car_melero($tiradas){
-        $this->$destreza = $this->$tiradas[0];
-        $this->$constitucion = $this->$tiradas[1];
-        $this->$inteligencia = $this->$tiradas[2];
-        $this->$presencia = $this->$tiradas[3];
-        $this->$sentidos = $this->$tiradas[4];                
+        $this->destreza = $tiradas[0];
+        $this->constitucion = $tiradas[1];
+        $this->inteligencia = $tiradas[2];
+        $this->presencia = $tiradas[3];
+        $this->sentidos = $tiradas[4];                
     }
     function car_distancia($tiradas){
-        $this->$destreza = $this->$tiradas[4];
-        $this->$constitucion = $this->$tiradas[2];
-        $this->$inteligencia = $this->$tiradas[1];
-        $this->$presencia = $this->$tiradas[3];
-        $this->$sentidos = $this->$tiradas[0];                
+        $this->destreza = $tiradas[4];
+        $this->constitucion = $tiradas[2];
+        $this->inteligencia = $tiradas[1];
+        $this->presencia = $tiradas[3];
+        $this->sentidos = $tiradas[0];                
     }
     function car_mental($tiradas){
-        $this->$destreza = $this->$tiradas[3];
-        $this->$constitucion = $this->$tiradas[4];
-        $this->$inteligencia = $this->$tiradas[1];
-        $this->$presencia = $this->$tiradas[0];
-        $this->$sentidos = $this->$tiradas[2];                
+        $this->destreza = $tiradas[3];
+        $this->constitucion = $tiradas[4];
+        $this->inteligencia = $tiradas[1];
+        $this->presencia = $tiradas[0];
+        $this->sentidos = $tiradas[2];                
     }
     
     function imprimir_pj(){
         echo ("<h1>Características</h1>");
         echo ("<ul>");
-        echo ("<li>Destreza: </li>" . $this->$destreza);
-        echo ("<li>Constitución: </li>" . $this->$constitucion);
-        echo ("<li>Inteligencia: </li>" . $this->$inteligencia);
-        echo ("<li>Presencia: </li>" . $this->$presencia);
-        echo ("<li>Sentidos: </li>" . $this->$sentidos);
+        echo ("<li>Destreza: " . $this->destreza) . "</li>";
+        echo ("<li>Constitución: " . $this->constitucion) . "</li>";
+        echo ("<li>Inteligencia: " . $this->inteligencia) . "</li>";
+        echo ("<li>Presencia: " . $this->presencia) . "</li>";
+        echo ("<li>Sentidos: " . $this->sentidos) . "</li>";
         echo ("</ul>");
     }
 }
