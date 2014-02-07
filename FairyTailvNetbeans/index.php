49da449f9f4b1ddff91f0ff875f2fd57bde83449
ladrillos.azules <ladrillos.azules@gmail.com>
2014-02-07 16:29:18 +0000

    Cada vez que se recargaba la página con F5 me daba un error, porque no había ninguna selección en el formulario. Para evitar andarme con sobrecargas del constructor, lo que he hecho es que por defecto al recargar el index (usando F5 en vez de dándole a crear) siempre me genere un melero.
    Por otro lado, he eliminado main.php, porque considero que (por el momento) no es necesario andarnos con presentaciones.

--- FairyTailvNetbeans/index.php
+++ FairyTailvNetbeans/index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link href='http://fonts.googleapis.com/css?family=Artifika' rel='stylesheet' type='text/css'>        
+        <link href='http://fonts.googleapis.com/css?family=Artifika' rel='stylesheet' type='text/css'>
         <meta charset="UTF-8">
         <title>Creación de Personajes de Fairy Tail</title>
         <link rel=stylesheet type="text/css" href="CSS/style.css">
@@ -14,19 +14,36 @@ and open the template in the editor.
     </head>
     <body>
         <div>
-            <h1>¡Crea un Personaje!</h1>
-        </div>
-        <div>            
-            <form action="main.php" method="post">                
+            <h1>¡Configura tu Personaje!</h1>
+            <form action="index.php" method="post">                
                 <p>
                     <select name="tipo_pj">
-                        <option>Melero</option>
-                        <option>Distancia</option>
-                        <option>Mental</option>
+                        <option value="Melero">Melero</option>
+                        <option value="Distancia">Distancia</option>
+                        <option value="Mental">Mental</option>
                     </select>
                     <input type="submit" value="¡Crear!" />
                 </p>
-            </form>            
+            </form>
+            
+        </div>
+        <div>
+            <h1>Personaje Creado</h1>
+        <?php
+        if (isset($_POST['tipo_pj'])){
+            $tipo_pj = ($_POST['tipo_pj']);
+        } else {
+            $tipo_pj='Melero';
+        }
+        include 'class_caracteristicas.php';
+        include 'class_trasfondos.php';
+        // include 'class_levear.php';
+        // include 'class_hechizos.php';
+        $caracteristicas = new class_caracteristicas($tipo_pj);
+        $trasfondos = new class_trasfondos();
+        // $levear = new levear();
+        // $hechizos = new hechizos();
+        ?>
         </div>
     </body>
 </html>
